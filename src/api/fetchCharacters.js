5e41264d71ec72e5fdbdf81b0e2f07189610043f
Albert Gomis <albert.gomis.20@gmail.com>
2023-07-17 11:00:53 +0200

    fetch changes

--- src/api/fetchCharacters.js
+++ src/api/fetchCharacters.js
@@ -1,7 +1,7 @@
 
 const fetchCharacters = async () =>{
     try {
-      const request = await fetch(`https://gateway.marvel.com:443/v1/public/characters?apikey=c3539cb9d6399d31be99ff2f6b0f6aa1`);
+      const request = await fetch(`https://gateway.marvel.com:443/v1/public/characters?limit=100&offset=100&apikey=c3539cb9d6399d31be99ff2f6b0f6aa1`);
       const response = await request.json();
       console.log(response) ;
     } catch (error) {
